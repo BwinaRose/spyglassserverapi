4ba150aeb43d21d9d36a276c88842195b80da5f1
BwinaRose <sabrinakrueger24@gmail.com>
2022-08-03 14:19:04 -0400

    server init

---
+++ src/main/java/the_thundercats/spyglassserverapi/SpyglassserverapiApplication.java
@@ -0,0 +1,13 @@
+package the_thundercats.spyglassserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyglassserverapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpyglassserverapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/the_thundercats/spyglassserverapi/SpyglassserverapiApplicationTests.java
@@ -0,0 +1,13 @@
+package the_thundercats.spyglassserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyglassserverapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
