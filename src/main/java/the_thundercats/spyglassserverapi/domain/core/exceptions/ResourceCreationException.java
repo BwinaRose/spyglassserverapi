6b542b73eced6890263847fe9f4148d435589abb
BwinaRose <sabrinakrueger24@gmail.com>
2022-08-04 10:48:16 -0400

    added exceptions

--- src/main/java/the_thundercats/spyglassserverapi/domain/core/exceptions/ResourceCreationException.java
+++ src/main/java/the_thundercats/spyglassserverapi/domain/core/exceptions/ResourceCreationException.java
@@ -1,4 +1,7 @@
 package the_thundercats.spyglassserverapi.domain.core.exceptions;
 
-public class ResourceCreationException {
+public class ResourceCreationException extends Exception{
+    public ResourceCreationException(String message) {
+        super(message);
+    }
 }
--- src/main/java/the_thundercats/spyglassserverapi/domain/core/exceptions/ResourceNotFoundException.java
+++ src/main/java/the_thundercats/spyglassserverapi/domain/core/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package the_thundercats.spyglassserverapi.domain.core.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
