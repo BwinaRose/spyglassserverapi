a70e8df5c2d05618c10e76389415cea6e8e88aea
BwinaRose <sabrinakrueger24@gmail.com>
2022-08-04 14:29:52 -0400

    User CSR completed

--- src/main/java/the_thundercats/spyglassserverapi/domain/serviceimpl/RecurringGoalServiceImpl.java
+++ src/main/java/the_thundercats/spyglassserverapi/domain/serviceimpl/RecurringGoalServiceImpl.java
@@ -1,68 +1,68 @@
-package the_thundercats.spyglassserverapi.domain.serviceimpl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import the_thundercats.spyglassserverapi.domain.core.exceptions.ResourceCreationException;
-import the_thundercats.spyglassserverapi.domain.core.exceptions.ResourceNotFoundException;
-import the_thundercats.spyglassserverapi.domain.models.RecurringGoal;
-import the_thundercats.spyglassserverapi.domain.models.User;
-import the_thundercats.spyglassserverapi.domain.repos.RecurringGoalRepo;
-import the_thundercats.spyglassserverapi.domain.services.RecurringGoalService;
-import the_thundercats.spyglassserverapi.domain.services.UserService;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class RecurringGoalServiceImpl implements RecurringGoalService {
-
-    private RecurringGoalRepo recurringGoalRepo;
-    private UserService userService;
-
-    @Autowired
-    public RecurringGoalServiceImpl(RecurringGoalRepo recurringGoalRepo, UserService userService) {
-        this.recurringGoalRepo = recurringGoalRepo;
-        this.userService = userService;
-    }
-
-    @Override
-    public RecurringGoal create(Long userId, RecurringGoal recurringGoal) throws ResourceNotFoundException {
-        User user = userService.getById(userId);
-        recurringGoal.setUser(user);
-        return recurringGoalRepo.save(recurringGoal);
-    }
-
-    @Override
-    public RecurringGoal getById(Long id) throws ResourceNotFoundException {
-        Optional<RecurringGoal> recurringGoalOptional = recurringGoalRepo.findById(id);
-        if(recurringGoalOptional.isEmpty()) {
-            throw new ResourceNotFoundException("Recurring goal not found with id: " + id);
-        }
-        return recurringGoalOptional.get();
-    }
-
-    @Override
-    public List<RecurringGoal> getAllFromUser(Long userId) throws ResourceNotFoundException {
-        return recurringGoalRepo.findByUser(userId);
-    }
-
-    @Override
-    public RecurringGoal update(Long goalId, RecurringGoal goalDetails) throws ResourceNotFoundException {
-        RecurringGoal recurringGoal = getById(goalId);
-        recurringGoal.setNameOfGoal(goalDetails.getNameOfGoal());
-        recurringGoal.setDescriptionOfGoal(goalDetails.getDescriptionOfGoal());
-        recurringGoal.setIconPicture(goalDetails.getIconPicture());
-        recurringGoal.setStartDate(goalDetails.getStartDate());
-        recurringGoal.setEndDate(goalDetails.getEndDate());
-        recurringGoal.setCurrentDollarAmount(goalDetails.getCurrentDollarAmount());
-        recurringGoal.setStartingDollarAmount(goalDetails.getStartingDollarAmount());
-        recurringGoal.setTargetDollarAmount(goalDetails.getTargetDollarAmount());
-        return recurringGoalRepo.save(recurringGoal);
-    }
-
-    @Override
-    public void delete(Long goalId) throws ResourceNotFoundException {
-        RecurringGoal recurringGoal = getById(goalId);
-        recurringGoalRepo.delete(recurringGoal);
-    }
-}
+//package the_thundercats.spyglassserverapi.domain.serviceimpl;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import the_thundercats.spyglassserverapi.domain.core.exceptions.ResourceCreationException;
+//import the_thundercats.spyglassserverapi.domain.core.exceptions.ResourceNotFoundException;
+//import the_thundercats.spyglassserverapi.domain.models.RecurringGoal;
+//import the_thundercats.spyglassserverapi.domain.models.User;
+//import the_thundercats.spyglassserverapi.domain.repos.RecurringGoalRepo;
+//import the_thundercats.spyglassserverapi.domain.services.RecurringGoalService;
+//import the_thundercats.spyglassserverapi.domain.services.UserService;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class RecurringGoalServiceImpl implements RecurringGoalService {
+//
+//    private RecurringGoalRepo recurringGoalRepo;
+//    private UserService userService;
+//
+//    @Autowired
+//    public RecurringGoalServiceImpl(RecurringGoalRepo recurringGoalRepo, UserService userService) {
+//        this.recurringGoalRepo = recurringGoalRepo;
+//        this.userService = userService;
+//    }
+//
+//    @Override
+//    public RecurringGoal create(Long userId, RecurringGoal recurringGoal) throws ResourceNotFoundException {
+//        User user = userService.getById(userId);
+//        recurringGoal.setUser(user);
+//        return recurringGoalRepo.save(recurringGoal);
+//    }
+//
+//    @Override
+//    public RecurringGoal getById(Long id) throws ResourceNotFoundException {
+//        Optional<RecurringGoal> recurringGoalOptional = recurringGoalRepo.findById(id);
+//        if(recurringGoalOptional.isEmpty()) {
+//            throw new ResourceNotFoundException("Recurring goal not found with id: " + id);
+//        }
+//        return recurringGoalOptional.get();
+//    }
+//
+//    @Override
+//    public List<RecurringGoal> getAllFromUser(Long userId) throws ResourceNotFoundException {
+//        return recurringGoalRepo.findByUser(userId);
+//    }
+//
+//    @Override
+//    public RecurringGoal update(Long goalId, RecurringGoal goalDetails) throws ResourceNotFoundException {
+//        RecurringGoal recurringGoal = getById(goalId);
+//        recurringGoal.setNameOfGoal(goalDetails.getNameOfGoal());
+//        recurringGoal.setDescriptionOfGoal(goalDetails.getDescriptionOfGoal());
+//        recurringGoal.setIconPicture(goalDetails.getIconPicture());
+//        recurringGoal.setStartDate(goalDetails.getStartDate());
+//        recurringGoal.setEndDate(goalDetails.getEndDate());
+//        recurringGoal.setCurrentDollarAmount(goalDetails.getCurrentDollarAmount());
+//        recurringGoal.setStartingDollarAmount(goalDetails.getStartingDollarAmount());
+//        recurringGoal.setTargetDollarAmount(goalDetails.getTargetDollarAmount());
+//        return recurringGoalRepo.save(recurringGoal);
+//    }
+//
+//    @Override
+//    public void delete(Long goalId) throws ResourceNotFoundException {
+//        RecurringGoal recurringGoal = getById(goalId);
+//        recurringGoalRepo.delete(recurringGoal);
+//    }
+//}
--- src/main/java/the_thundercats/spyglassserverapi/domain/services/UserService.java
+++ src/main/java/the_thundercats/spyglassserverapi/domain/services/UserService.java
@@ -1,6 +1,12 @@
 package the_thundercats.spyglassserverapi.domain.services;
 
+import the_thundercats.spyglassserverapi.domain.core.exceptions.ResourceCreationException;
+import the_thundercats.spyglassserverapi.domain.core.exceptions.ResourceNotFoundException;
+import the_thundercats.spyglassserverapi.domain.models.User;
+
 public interface UserService {
-
-
+    User createUser(User user) throws ResourceCreationException;
+    User getUserById(String id) throws ResourceNotFoundException;
+    User updateUser(String id, User userDetails) throws ResourceNotFoundException,ResourceCreationException;
+    void deleteUser(String id) throws ResourceNotFoundException;
 }
